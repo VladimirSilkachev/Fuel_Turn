dec04d3e15749350c635554136f4e1824dc877d7
VladimirSilkachev <79515746577@yandex.ru>
2020-04-16 17:01:32 +0700

    start

---
+++ main.py
@@ -0,0 +1,21 @@
+"""Project-Case: TurnFuel
+   Developers: Silkachev Vinnikov Popov"""
+d = {}
+
+
+def main():
+    with open('input.txt', 'r') as f:
+        a = f.readlines()
+    pass
+
+
+def chose_column():  # Выбор колонки, составление словаря тут.
+    pass
+
+
+def time():  # Хз надо ли, можно просто в мэйн пихнуть, считает время.
+    pass
+
+
+def print_():  # Выводит хрень мол кто куда встал и насколько что занято.
+    pass
